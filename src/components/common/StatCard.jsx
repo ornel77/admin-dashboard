26b548b867ff5d32dc232baa037ec7c237566991
ornel77 <ornella.tomboza@gmail.com>
2024-07-31 16:55:11 +0200

    first commit

---
+++ src/components/common/StatCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+const StatCard = ({ name, icon: Icon, value, color }) => {
+  return (
+    <motion.div
+      className=""
+      whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
+    >
+      <div className="px-4 py-5 sm:p-6">
+        <span className="flex items-center text-sm font-medium text-gray-400">
+          <Icon size={20} className="mr-2" style={{ color }} />
+          {name}
+        </span>
+        <p className="mt-1 text-3xl font-semibold text-gray-100">{value}</p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default StatCard;
