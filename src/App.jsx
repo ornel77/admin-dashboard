f3269f6741346968b32918a0300f667bad5e82da
ornel77 <ornella.tomboza@gmail.com>
2024-08-02 16:34:21 +0200

    analytics ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar";
 import UsersPage from "./pages/UsersPage";
 import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
         <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
       </Routes>
     </div>
   );
